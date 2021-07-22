84b9a94d8dcf8664473f388e5a246c8929e53176
Rohit Patil <superfissionr45@gmail.com>
2021-07-22 17:40:10 +0530

    Added comments

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,5 +1,4 @@
 import cv2
-import time
 import mediapipe as mp
 
 
@@ -45,8 +44,6 @@ class handDetector():
 
 
 def main():
-    pTime = 0
-    cTime = 0
     cap = cv2.VideoCapture(0)
     detector = handDetector()
     while True:
@@ -56,13 +53,6 @@ def main():
         #print(lmList)
         if len(lmList) != 0:
             print(lmList[4])
-
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
-                    (255, 0, 255), 3)
 
         cv2.imshow("Image", img)
         cv2.waitKey(1)
